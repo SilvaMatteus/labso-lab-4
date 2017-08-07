c76f50ffe42d3a18643a933015027fa926a1ba9d
Fábio Silva <fabio.fernando.osilva@gmail.com>
2017-08-07 14:26:38 -0300

    Adiciona algoritmo FIFO

--- python/phymem.py
+++ python/phymem.py
@@ -7,31 +7,65 @@
 # NOTE: there may be methods you don't need to modify, you must decide what
 # you need...
 
+from Queue import Queue
+
+
 class PhysicalMemory:
-  ALGORITHM_AGING_NBITS = 8
-  """How many bits to use for the Aging algorithm"""
+    ALGORITHM_AGING_NBITS = 8
+    """How many bits to use for the Aging algorithm"""
 
-  def __init__(self, algorithm):
-    assert algorithm in {"fifo", "nru", "aging", "second-chance"}
-    self.algorithm = algorithm
+    def __init__(self, algorithm):
+        assert algorithm in {"fifo", "nru", "aging", "second-chance"}
+        self.algorithm = algorithm
 
-  def put(self, frameId):
-    """Allocates this frameId for some page"""
-    # Notice that in the physical memory we don't care about the pageId, we only
-    # care about the fact we were requested to allocate a certain frameId
-    pass
+        if algorithm == "fifo":
+            self.impl = FIFOAlgorithm()
 
-  def evict(self):
-    """Deallocates a frame from the physical memory and returns its frameId"""
-    # You may assume the physical memory is FULL so we need space!
-    # Your code must decide which frame to return, according to the algorithm
-    pass
+    def put(self, frameId):
+        """Allocates this frameId for some page"""
+        # Notice that in the physical memory we don't care about the pageId, we only
+        # care about the fact we were requested to allocate a certain frameId
+        self.impl.put(frameId)
 
-  def clock(self):
-    """The amount of time we set for the clock has passed, so this is called"""
-    # Clear the reference bits (and/or whatever else you think you must do...)
-    pass
 
-  def access(self, frameId, isWrite):
-    """A frameId was accessed for read/write (if write, isWrite=True)"""
-    pass
+    def evict(self):
+        """Deallocates a frame from the physical memory and returns its frameId"""
+        # You may assume the physical memory is FULL so we need space!
+        # Your code must decide which frame to return, according to the algorithm
+        return self.impl.evict()
+
+    def clock(self):
+        """The amount of time we set for the clock has passed, so this is called"""
+        # Clear the reference bits (and/or whatever else you think you must do...)
+        self.impl.clock()
+
+    def access(self, frameId, isWrite):
+        """A frameId was accessed for read/write (if write, isWrite=True)"""
+        self.impl.access(frameId, isWrite)
+
+
+class FIFOAlgorithm:
+    def __init__(self):
+        self.allocatedFrames = Queue()
+
+    def put(self, frameId):
+        """Allocates this frameId for some page"""
+        # Notice that in the physical memory we don't care about the pageId, we only
+        # care about the fact we were requested to allocate a certain frameId
+        self.allocatedFrames.put(frameId)
+        pass
+
+    def evict(self):
+        """Deallocates a frame from the physical memory and returns its frameId"""
+        # You may assume the physical memory is FULL so we need space!
+        # Your code must decide which frame to return, according to the algorithm
+        return self.allocatedFrames.get()
+
+    def clock(self):
+        """The amount of time we set for the clock has passed, so this is called"""
+        # Clear the reference bits (and/or whatever else you think you must do...)
+        pass
+
+    def access(self, frameId, isWrite):
+        """A frameId was accessed for read/write (if write, isWrite=True)"""
+        pass
